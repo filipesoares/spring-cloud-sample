1bbcfc62fa6fac2d45cb6ba7d4a0a2ee97c94e62
Filipe Soares de Oliveira <filipe.oliveira@radixeng.com.br>
2020-01-30 16:47:16 -0300

    Remove unused directories

--- accounts-api/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./src/config/server')
-
-app.listen(3000, function () {
-  console.log('server running...')
-})
--- accounts-api/src/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-const baseRoutes = require('./base-routes')
-const accountRoutes = require('./account-routes')
-const contextRoot = '/financer/_api/v1'
-
-module.exports = app => {
-  baseRoutes(app, contextRoot)
-  accountRoutes(app, contextRoot)
-}
